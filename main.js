ac7e0ae56057a7b85dfdf818f0b88f2d1413da2d
jean-celeste <125885532+jean-celeste@users.noreply.github.com>
2023-05-27 01:51:01 +0800

    working contact form

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const parallax_home = document.getElementById("home");
-const parallax_homeContent = document.getElementById("homeCont");
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
     home.style.backgroundPositionY = offset * 0.7 + "px";
 
-})
+});
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(e){
+    e.preventDefault()
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    var body = 'Name:' +name +'<br/> email: ' +email + '<br/> Message: ' +message;
+
+    Email.send({
+        SecureToken : "e2efcd08-10e7-4a28-9c07-a97f3b865a7c",
+        To : 'jeancarlotarrobago.celeste@bicol-u.edu.ph',
+        Subject: "From Website",
+        From : email,
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+});
